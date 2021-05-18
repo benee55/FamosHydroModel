157d4bf0426dd54663dc021c7f40db5f221c5748
benee55 <benlee55@gmail.com>
2021-05-18 15:11:34 -0400

    run file

--- Official_Fast/precalibration/run/run.R
+++ Official_Fast/precalibration/run/run.R
@@ -7,13 +7,8 @@ outputDir<-"/gpfs/group/kzk10/default/private/hydrocalib/SGrove/famos/Official_F
 # Precalibration
 ensembleN<-100
 parMat<-apply(boundMat,1,function(x,ens){runif(ens,min=x[1],max=x[2])},ens=ensembleN)
+parMat<-cbind(rinvgamma(ensembleN,shape = priorPar[1,1], rate = priorPar[1,2]),parMat)
 # Write Input
 j=1
-writeInput( par = parMat[j,] , j = j , dir = inputDir)# Write Input
-writeOutput( j = j , dir = outputDir) # Write Output
-pt<-proc.time()
-# runHydroModel( j = j , dir = inputDir) # Run Model
-system(paste("/gpfs/group/kzk10/default/private/hydrocalib/SGrove/bin/rdhm ",
-             "/gpfs/group/kzk10/default/private/hydrocalib/SGrove/famos/Official_Fast/precalibration/input/calsnow.card"))
-ptFinal<-proc.time()-pt
-save(ptFinal, file = "FinalTime.RData")
+dat<-modelEval( par=parMat[j,], j=j , inputDir=inputDir , outputDir=outputDir)
+save(dat,file = "FinalTime.RData")
