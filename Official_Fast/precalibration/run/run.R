17c4d6eef9427692552815bb899dd920208e0fd4
benee55 <benlee55@gmail.com>
2021-05-18 16:08:58 -0400

    large sample

--- Official_Fast/precalibration/run/run.R
+++ Official_Fast/precalibration/run/run.R
@@ -5,17 +5,17 @@ source("../run/rWrapper_Continuous.R")
 source("../run/mcmc_source_Tr.R")
 
 # Precalibration
-ensembleN<-60
+ensembleN<-1000
 parMat<-apply(boundMat,1,function(x,ens){runif(ens,min=x[1],max=x[2])},ens=ensembleN)
 parMat<-cbind(rinvgamma(ensembleN,shape = priorPar[1,1], rate = priorPar[1,2]),parMat)
 save(parMat, file="output/mhParameters_0.RData")
 
-ens<-nprocs <-39
+nprocs <-20*10-1
 mp_type = "MPI" # PSOCK or MPI
 cl <- parallel::makeCluster(spec = nprocs, type="MPI")
 doParallel::registerDoParallel(cl)
 
-outputMat<-foreach::foreach(jobNum=1:ens) %dopar% {
+outputMat<-foreach::foreach(jobNum=1:ensembleN) %dopar% {
   source("../run/rWrapper_Continuous.R")
   source("../run/mcmc_source_Tr.R")
   load("output/mhParameters_0.RData")
