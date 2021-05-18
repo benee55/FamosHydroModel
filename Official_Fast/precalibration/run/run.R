30a6c8fea656619ae671df9b384589dae9a7610c
benee55 <benlee55@gmail.com>
2021-05-18 13:18:42 -0400

    new

--- Official_Fast/precalibration/run/run.R
+++ Official_Fast/precalibration/run/run.R
@@ -2,13 +2,14 @@ rm(list=ls())
 setwd("/gpfs/group/kzk10/default/private/hydrocalib/SGrove/famos/Official_Fast/precalibration")
 source("../run/rWrapper_Continuous.R")
 source("../run/mcmc_source_Tr.R")
-inputDir<-"~/Dropbox/hydroFamos/run/precalibration/input"
-outputDir<-"~/Dropbox/hydroFamos/run/precalibration/output"
+inputDir<-"/gpfs/group/kzk10/default/private/hydrocalib/SGrove/famos/Official_Fast/precalibration/input"
+outputDir<-"/gpfs/group/kzk10/default/private/hydrocalib/SGrove/famos/Official_Fast/precalibration/output"
 # Precalibration
 ensembleN<-100
 parMat<-apply(boundMat,1,function(x,ens){runif(ens,min=x[1],max=x[2])},ens=ensembleN)
 # Write Input
-writeInput( par = parMat[1,] , j = j , dir = inputDir)# Write Input
+j=1
+writeInput( par = parMat[j,] , j = j , dir = inputDir)# Write Input
 writeOutput( j = j , dir = outputDir) # Write Output
 pt<-proc.time()
 runHydroModel( j = j , dir = inputDir) # Run Model
