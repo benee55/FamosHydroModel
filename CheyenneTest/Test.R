871ae3ae18cabb45196384402a7d8df0ccd913e3
benee55 <benlee55@gmail.com>
2021-05-25 17:17:14 -0400

    largescale

--- CheyenneTest/Test.R
+++ CheyenneTest/Test.R
@@ -9,8 +9,8 @@ print("Made Cluster")
 doParallel::registerDoParallel(cl)
 print("Registered Cluster")
 pt<-proc.time()
-outputMat<-foreach::foreach(jobNum=1:(ns*10), .combine = "c") %dopar% {
-  Sys.sleep(10)
+outputMat<-foreach::foreach(jobNum=1:(ns*4), .combine = "c") %dopar% {
+  Sys.sleep(2)
   jobNum
   }
 ptFinal<-proc.time()-pt
