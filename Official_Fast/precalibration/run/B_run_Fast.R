2cc5a7dfd24288ed80887780d2f29ed17e765270
benee55 <benlee55@gmail.com>
2021-05-26 03:16:05 -0400

    large

--- Official_Fast/precalibration/run/B_run_Fast.R
+++ Official_Fast/precalibration/run/B_run_Fast.R
@@ -5,21 +5,21 @@ setwd("/glade/u/home/sanjib/FamosHydroModel/Official_Fast/precalibration")
 nprocs <-mpi.universe.size() - 1
 print(nprocs)
 
-# mp_type = "MPI"
-# cl <- parallel::makeCluster(spec = nprocs, type=mp_type)
-# doParallel::registerDoParallel(cl)
-# 
-# # Values for runs
-# runIndex<-1:(nprocs*2)
-# outputMat<-foreach::foreach(jobNum=runIndex , .combine = "cbind" , .packages = c("mvtnorm","tmvtnorm","invgamma")) %dopar% {
-#   source("../run/rWrapper_Continuous_Fast.R")
-#   source("../run/mcmc_source_Tr.R")
-#   load(file="output/mhParameters_0.RData")
-#   inputDir<-"/glade/scratch/sanjib/fastTest/input"
-#   outputDir<-"/glade/scratch/sanjib/fastTest/output"
-#   jobPar<-parMat[jobNum,]
-#   modelEval(par = jobPar , j = jobNum , inputDir =inputDir , outputDir = outputDir)
-# }
-# 
-# save(outputMat,file = "output/fast_preCalibrationResults.RData")
-# 
+mp_type = "MPI"
+cl <- parallel::makeCluster(spec = nprocs, type=mp_type)
+doParallel::registerDoParallel(cl)
+
+# Values for runs
+runIndex<-1:(nprocs*2)
+outputMat<-foreach::foreach(jobNum=runIndex , .combine = "cbind" , .packages = c("mvtnorm","tmvtnorm","invgamma")) %dopar% {
+  source("../run/rWrapper_Continuous_Fast.R")
+  source("../run/mcmc_source_Tr.R")
+  load(file="output/mhParameters_0.RData")
+  inputDir<-"/glade/scratch/sanjib/fastTest/input"
+  outputDir<-"/glade/scratch/sanjib/fastTest/output"
+  jobPar<-parMat[jobNum,]
+  modelEval(par = jobPar , j = jobNum , inputDir =inputDir , outputDir = outputDir)
+}
+
+save(outputMat,file = "output/fast_preCalibrationResults.RData")
+
