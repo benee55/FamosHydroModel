18a52006750a3c70d930faa22d34a4125194b7d0
benee55 <benlee55@gmail.com>
2021-05-26 00:15:03 -0400

    update

--- Official_Fast/precalibration/run/run_single.R
+++ Official_Fast/precalibration/run/run_single.R
@@ -6,8 +6,8 @@ source("../run/mcmc_source_Tr.R")
 source("../run/rWrapper_Continuous.R")
 source("../run/mcmc_source_Tr.R")
 load("output/mhParameters_0.RData")
-inputDir<-"/glade/scratch/sanjib/precalibration/input"
-outputDir<-"/glade/scratch/sanjib/precalibration/output"
+inputDir<-"/glade/u/home/sanjib/FamosHydroModel/Official_Fast/precalibration/input"
+outputDir<-"/glade/u/home/sanjib/FamosHydroModel/Official_Fast/precalibration/output"
 jobNum=1
 jobPar<-parMat[jobNum,]
 outputMat<- modelEval( par = jobPar , j = jobNum , inputDir =inputDir , outputDir = outputDir)
