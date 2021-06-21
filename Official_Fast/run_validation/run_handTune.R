8f37415e7fec0f36c3104d987c39e1bbfa8bdbce
benee55 <benlee55@gmail.com>
2021-06-20 20:31:56 -0400

    handTune

--- Official_Fast/run_validation/run_handTune.R
+++ Official_Fast/run_validation/run_handTune.R
@@ -1,6 +1,6 @@
 setwd("/glade/u/home/sanjib/FamosHydroModel/Official_Fast")
 
-source("/run/mcmc_source_Tr.R")
+source("run/mcmc_source_Tr.R")
 inputDir<-"/glade/scratch/sanjib/validation/input/handTune"
 outputDir<-"/glade/scratch/sanjib/validation/output/handTune"
 jobPar<-c(2, # Variance - Not important in this evaluation 
@@ -17,9 +17,9 @@ jobPar<-c(2, # Variance - Not important in this evaluation
               3.0 , # rutpix_Q0CHN
               1.0) # rutpix_QMCHN
 
-source("/run_validation/rWrapper.R")
+source("run_validation/rWrapper.R")
 outputMat<-modelEval(par = jobPar , j = 1 , inputDir =inputDir , outputDir = outputDir)
 save(outputMat , file = "output_validation/handTuneResults.RData")
-source("/run/rWrapper_Continuous.R")
+source("run/rWrapper_Continuous.R")
 modelRun<-modelEval(par = jobPar , j = 2 , inputDir =inputDir , outputDir = outputDir)
 save(outputMat , modelRun,file = "output_validation/handTuneResults.RData")
