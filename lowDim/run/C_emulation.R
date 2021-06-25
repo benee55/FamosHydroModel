d0f7127ff511babfc1a1057d7a61cd369a08d920
benee55 <benlee55@gmail.com>
2021-06-25 03:14:22 -0400

    projection

--- lowDim/run/C_emulation.R
+++ lowDim/run/C_emulation.R
@@ -8,12 +8,21 @@ modelRuns<-matrix(NA,nrow=length(outputMat[1,]), ncol=length(unlist(outputMat[1,
 for(k in 1:length(outputMat[1,])){
   modelRuns[k,]<-  unlist(outputMat[1,k])
 }
-summary(modelRuns)
+# summary(modelRuns)
+
+load("output/testModelRuns.RData")
+testRuns<-matrix(NA,nrow=length(outputMat[1,]), ncol=length(unlist(outputMat[1,1])))
+for(k in 1:length(outputMat[1,])){
+  testRuns[k,]<-  unlist(outputMat[1,k])
+}
+# summary(testRuns)
+
 
 # load Design Matrix
 load("input/design.RData")
-
+parMat<-round(parMat,3)
 # Build Emulator via paralellization
+if(FALSE){
 # Install on cheyenne by loading gnu module
 library(snow);library(snowfall);library(mlegp)
 sfInit(parallel=TRUE, cpus=7, type='PSOCK')
@@ -36,25 +45,73 @@ sfStop()
 
 save(gpEmulator,gpEmulator_CM, modelRuns, parMat, 
      file="output/GPEmulator_Full.RData")  
-
+}
 
 
 # Try out Emulator
 load("output/GPEmulator_Full.RData")
+
+# PRedict on test parameters
+predTest_CM<-predTest<-matrix(NA, nrow=nrow(testMat) , ncol=21)
+for(k in 1:21){
+  print(k)
+  predTest[,k]<-apply(testMat, 1, predict, object=gpEmulator[[k]])
+  predTest_CM[,k]<-apply(testMat, 1, predict, object=gpEmulator_CM[[k]])
+}
+#Validation
+rmse<-sqrt(apply((testRuns-predTest)^2,1,mean))
+rmse_CM<-sqrt(apply((testRuns-predTest_CM)^2,1,mean))
+mean(rmse)
+mean(rmse_CM)
+
+
+dev.off()
+# sampInd<-sample(1:71, 25)
+sampInd<-order(rmse,decreasing = TRUE)[1:25]
+par(mfrow=c(5,5),mar=c(2,2,2,2))
+for(k in 1:length(sampInd)){
+  hj<-sampInd[k]
+plot(x=1:21, y=predTest[hj,],typ="l" , 
+     ylim=range(predTest[hj,],testRuns[hj,]), 
+     main=paste("Test Run", hj))
+# lines(x=1:21, y=predTest_CM[1,], col="red")
+points(x=1:21, y=predTest[hj,], pch=16, col="black")
+# points(x=1:21, y=predTest_CM[1,], pch=16, col="red")
+lines(x=1:21, y=testRuns[hj,], col="blue")
+points(x=1:21, y=testRuns[hj,], pch=16, col="blue")
+# if(k==1){
+#   legend("topright", legend=c("Truth","Emulation"), lty=c(1,1) , 
+#          col=c("blue","black"))
+# }
+
+}
+
+apply(parMat,2,unique)
+keepInd<-which(parMat[,3]==2.5 &parMat[,4]==1.1)
+bar<-parMat[keepInd,1:2]
+baz<-modelRuns[keepInd,1]
+source("~/Dropbox/BenJaewoo/SMB_SGLMM/RevisionMaterials/Code/FullModis/source/sharedFunctions.R")
+library(fields); library(classInt)
+plotRF(dat=baz, location = bar)
+
+
+
 # First parameter
+apply(parMat,2,unique)
 testSeq<-seq(0,5, length.out = 100)
 resVect_CM<-resVect<-vector("numeric")
 for(j in 1:length(testSeq)){
   # print(j)
-  jobPar<-c(testSeq[j],2,3.5,1.1)
+  jobPar<-c(testSeq[j],1.0,2.5,1.1)
   resVect[j]<-predict(object = gpEmulator[[1]], newData = jobPar)
   resVect_CM[j]<-predict(object = gpEmulator_CM[[1]], newData = jobPar)
 }
-par(mfrow=c(1,2))
-plot(x=testSeq, y=resVect , typ="l" , ylim=range(resVect , resVect_CM));
+par(mfrow=c(1,1))
+plot(x=testSeq, y=resVect , typ="l" , ylim=range(resVect));
 lines(x=testSeq , y=resVect_CM, col="red")
-
-summary(parMat)
+apply(parMat,2,unique)
+foo<-which(round(parMat[,2],1)==1.0 & round(parMat[,3],1)==2.5 & round(parMat[,4],1)==1.1)
+points(x=parMat[foo,1] , y=modelRuns[foo,1])
 
 #load observations
 load("input/fullObservations.RData")
--- lowDim/run/D_mcmcEmulation.R
+++ lowDim/run/D_mcmcEmulation.R
@@ -1,25 +1,30 @@
+rm(list=ls())
+setwd("~/Dropbox/FamosHydroModel/lowDim/")
+# setwd("/glade/u/home/sanjib/FamosHydroModel/lowDim/")
+load("input/fullObservations.RData")
+load("output/GPEmulator_Full.RData")
+source("run/mcmc_source_Tr.R")
 # Log prior
 logPrior<-function(par, boundMat){
-  sum(dunif(x=par[-4],min=boundMat[1,],max=boundMat[2,],log = TRUE),
-      dinvgamma(x = par[4] , shape = 2, rate =2, log=TRUE))
+  sum(dunif(x=par[-5],min=boundMat[,1],max=boundMat[,2],log = TRUE),
+      dinvgamma(x = par[5] , shape = 2, rate =2, log=TRUE))
 }
 
-logLlhd<-function(par,boundMat,gpEmulator,Jy,dat,K){
-  coeffs<-predict(gpEmulator[[1]], newData = matrix(par[-4],nrow=1))
+logLlhd<-function(par,boundMat,gpEmulator,Jy,dat){
+  output<-predict(gpEmulator[[1]], newData = matrix(par[-5],nrow=1))
   for(i in 2:Jy){
-    coeffs<-c(coeffs,
-              predict(gpEmulator[[i]], newData = matrix(par[-4],nrow=1)))
+    output<-c(output,
+              predict(gpEmulator[[i]], newData = matrix(par[-5],nrow=1)))
   }
-  output<-as.numeric(K%*%coeffs)
   # Model Output
-  sum(dnorm(x=dat,mean=output,sd=par[4],log=TRUE))
+  sum(dnorm(x=dat,mean=output,sd=(par[5]),log=TRUE))
   
 }
 
 # Log Posterior
-logPost<-function(par,boundMat,gpEmulator,Jy,dat,K){
+logPost<-function(par,boundMat,gpEmulator,Jy,dat){
   
-  if(any(par[-4]<boundMat[1,]) | any(par[-4]>boundMat[2,]) | par[4]<0) {
+  if(any(par[-5]<boundMat[,1]) | any(par[-5]>boundMat[,2]) | par[5]<0) {
     lPost<- -Inf
   }else{
     lPrior<-logPrior(par=par , boundMat=boundMat)
@@ -27,8 +32,7 @@ logPost<-function(par,boundMat,gpEmulator,Jy,dat,K){
                   boundMat = boundMat,
                   gpEmulator = gpEmulator,
                   Jy = Jy,
-                  dat = dat,
-                  K = K)
+                  dat = dat)
     
     lPost<-lPrior+llhd
   }
@@ -40,19 +44,19 @@ logPost<-function(par,boundMat,gpEmulator,Jy,dat,K){
 library(adaptMCMC)
 accept.mcmc = 0.234										# Optimal acceptance rate as # parameters->infinity
 #	(Gelman et al, 1996; Roberts et al, 1997)
-niter.mcmc =25000										# number of iterations for MCMC
+niter.mcmc =100000										# number of iterations for MCMC
 gamma.mcmc = 0.55										# rate of adaptation (between 0.5 and 1, lower is faster adaptation)
 burnin = round(niter.mcmc*0.1)				# how much to remove for burn-in
 stopadapt.mcmc = round(niter.mcmc*5)
 
-par.init<-c(apply(theta.ori,2,mean),1)
-useDat<-fullUvicDat[,20]+rnorm(n=nTot,mean=0,sd=0.1)
+par.init<-c(apply(boundMat,1,mean),1000)
+useDat<-subsetFinalObs
 # Run MCMC
 pt<-proc.time()
 amcmc.out = MCMC(p=logPost, n=niter.mcmc, init=par.init, 
                  boundMat = boundMat,
                  gpEmulator = gpEmulator,
-                 Jy = Jy,dat = useDat,K=K,
+                 Jy = length(useDat),dat = useDat,
                  acc.rate=accept.mcmc,
                  gamma=gamma.mcmc, list=TRUE, 
                  n.start=round(0.01*niter.mcmc),
@@ -61,11 +65,13 @@ ptFinal<-proc.time()-pt
 ptFinal
 amcmc.out$acceptance.rate
 apply(amcmc.out$samples,2,mean)
-par(mfrow=c(4,2),mar=c(2,2,2,2))
-burnin=1000
-for(i in 1:4){
-  plot.ts(amcmc.out$samples[-(1:burnin),i])
-  abline(h=theta.ori[20,i],col="red")
-  plot(density(amcmc.out$samples[-(1:burnin),i]))
-  abline(v=theta.ori[20,i],col="red")
+par(mfrow=c(5,2),mar=c(2,2,2,2))
+burnin=niter.mcmc*0.25
+newParNames<-c(parNames,"s2")
+for(i in 1:5){
+  plot.ts(amcmc.out$samples[-(1:burnin),i], main=newParNames[i])
+  # abline(h=theta.ori[20,i],col="red")
+  plot(density(amcmc.out$samples[-(1:burnin),i]), main=newParNames[i])
+  # abline(v=theta.ori[20,i],col="red")
 }
+save(amcmc.out,file="output/mcmcResults.RData")
