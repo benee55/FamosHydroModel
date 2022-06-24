486c12ffc135766b8f0bc6fb9d2bff6054246315
benee55 <benlee55@gmail.com>
2022-06-24 12:01:51 -0400

    validaiton

--- Official_Fast/run_rep_var/validation/run_validation.R
+++ Official_Fast/run_rep_var/validation/run_validation.R
@@ -11,16 +11,16 @@ cl <- parallel::makeCluster(spec = nprocs, type=mp_type)
 doParallel::registerDoParallel(cl)
 
 # Values for runs
-runIndex<-1:1007
+runIndex<-1:2015
 outputMat<-foreach::foreach(jobNum=runIndex , .combine = "cbind" , .packages = c("mvtnorm","tmvtnorm","invgamma")) %dopar% {
-  load(file="output_rep/mhParameters_4.RData")
-  source("run_rep/mcmc_source_Tr.R")
-  inputDir<-"/glade/scratch/sanjib/validation/input/Famos_rep"
-  outputDir<-"/glade/scratch/sanjib/validation/output/Famos_rep"
+  load(file="output_rep_var/mhParameters_4.RData")
+  source("run_rep_var/mcmc_source_Tr.R")
+  inputDir<-"/glade/scratch/sanjib/validation/input"
+  outputDir<-"/glade/scratch/sanjib/validation/output"
   jobPar<-rep2orig(parMat[jobNum,])
   source("run_validation/rWrapper.R")
   modelEval(par = jobPar , j = jobNum , inputDir =inputDir , outputDir = outputDir)
 }
 
-save(outputMat,file ="output_validation/famosResults_rep.RData")
+save(outputMat,file ="output_rep_var/famosResults_rep_var.RData")
 
